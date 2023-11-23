e70418284d2832dbda5d48385f3431623e11a058
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-11-23 14:51:13 +0300

    feat: add goods

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -2,9 +2,22 @@ const newItemsContainer = document.querySelector('.new-items__container');
 
 const initSwiper = () => {
   const newItemsSwiper = new Swiper(newItemsContainer, {
+    // containerModifierClass: 'new-items__',
+    // wrapperClass: 'new-items__wrapper',
+    effect: 'coverflow',
+    // watchSlidesProgress: true,
+    centerInsufficientSlides: true,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 0,
+      modifier: 1,
+      // scale: 0.5,
+      slideShadows: false,
+    },
     navigation: {
-      nextEl: '.new-items__button--next',
-      prevEl: '.new-items__button--prev',
+      nextEl: '.new-items__swiper-button--next',
+      prevEl: '.new-items__swiper-button--prev',
     },
     breakpoints: {
       320: {
@@ -12,7 +25,7 @@ const initSwiper = () => {
         spaceBetween: 20,
       },
       768: {
-        slidesPerView: 3,
+        slidesPerView: 4,
         spaceBetween: 30,
       },
       1200: {
