2a21394ab4251622823fbc9538d1f8348017536e
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-11-24 14:39:17 +0300

    fix: fix swiper buttons

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -4,34 +4,33 @@ const promoContainer = document.querySelector('.promo__container');
 
 const initSwiper = () => {
   const newItemsSwiper = new Swiper(newItemsContainer, {
-    // containerModifierClass: 'new-items__',
-    // wrapperClass: 'new-items__wrapper',
-    effect: 'coverflow',
-    // watchSlidesProgress: true,
-    centerInsufficientSlides: true,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 0,
-      modifier: 1,
-      // scale: 0.5,
-      slideShadows: false,
-    },
     navigation: {
       nextEl: '.new-items__swiper-button--next',
       prevEl: '.new-items__swiper-button--prev',
     },
+    //centeredSlides: true,
+    //cssMode: true,
+
+    watchSlidesProgress: true,
+    //watchOverflow: true,
+    // simulateTouch: false,
+    // watchSlidesVisibility: true,
+    // effect: 'creative',
+    /* creativeEffect: {
+      perspective: false,
+    }*/
+	  //centerInsufficientSlides: true,
     breakpoints: {
       320: {
         slidesPerView: 2,
+        spaceBetween: 8,
+      },
+      375: {
+        slidesPerView: 'auto',
         spaceBetween: 20,
       },
       768: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-      1200: {
-        slidesPerView: 4,
+        slidesPerView: 'auto',
         spaceBetween: 30,
       },
     },
