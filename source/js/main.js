083fad0b6aba24daa7e7302e48e64d1900d95a8d
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-11-28 23:55:43 +0300

    feat: add catalog-sidebar

--- source/js/main.js
+++ source/js/main.js
@@ -2,8 +2,10 @@ import {initSwiper} from './modules/init-swiper.js';
 import {setModal} from './modules/set-modal.js';
 import {setContacts} from './modules/set-contacts.js';
 import {setSearch} from './modules/set-search.js';
+import {setCatalog} from './modules/set-catalog.js';
 
 initSwiper();
 setModal();
 setContacts();
 setSearch();
+setCatalog();
